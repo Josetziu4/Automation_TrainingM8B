9e9dcef7adf846aa8e1f74f1bd7d54fdf5804805
Carlos Paz <carlos.paz@4thsource.com>
2019-12-13 17:02:13 -0600

    OneTimeSetup

---
+++ LinkedIn_LoginPage/Base Files/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace LinkedIn_LoginPage.Base_Files
+{
+    class BaseTest
+    {
+        public IWebDriver driver;
+        public string url;
+        public string username;
+        public string password;
+
+        [OneTimeSetUp]
+        public void BeforeAlltests()
+        {
+            url = Environment.GetEnvironmentVariable("url",EnvironmentVariableTarget.User);
+            username = Environment.GetEnvironmentVariable("username", EnvironmentVariableTarget.User);
+            password = Environment.GetEnvironmentVariable("password", EnvironmentVariableTarget.User);
+        }
+    }
+}
