618b367548851791f3f14592d01d601a1ebd150f
Alexis Tun <alexis.tun@4thsource.com>
2019-12-17 17:00:56 -0600

    clase del 17-12-2019

---
+++ LinkedIn_LoginPage/Test Cases/LinkedIn_LoginTests.cs
@@ -0,0 +1,24 @@
+﻿using LinkedIn_LoginPage.Base_Files;
+using LinkedIn_LoginPage.Page_Objects;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedIn_LoginPage.Test_Cases
+{
+    class LinkedIn_LoginTests : BaseTest
+    {
+        public static LinkedIn_LoginPageModel objLogin;
+
+        [Test]
+        public void LinkedInLogin()
+        {
+            objLogin = new LinkedIn_LoginPageModel(driver);
+            objLogin.fnEnterUsername(username);
+
+        }
+    }
+}
