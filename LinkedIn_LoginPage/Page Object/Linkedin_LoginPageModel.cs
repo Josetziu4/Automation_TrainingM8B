599aeb8cc68f5494e7d7032c4cedae62cd9d2b89
manuelkumay <manuel.ku@4thsource.com>
2019-12-17 17:50:13 -0600

    Traning Test Page Object 12-17-19

---
+++ LinkedIn_LoginPage/Page Object/Linkedin_LoginPageModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedIn_LoginPage.Base_Files;
+using OpenQA.Selenium;
+
+namespace LinkedIn_LoginPage.Page_Object
+{
+    class Linkedin_LoginPageModel : BaseTest
+    {
+        //DRIVER REFERENCESS
+        private static IWebDriver _objDriver;
+
+        public Linkedin_LoginPageModel(IWebDriver driver)
+        {
+            _objDriver = driver;
+        }
+
+        //ELEMENT LOCATOR 
+        private static readonly string STR_USSERNAME_TEXTFIELD_ID = "username";
+        private static readonly string STR_PASSWORD_TEXTFIELD_ID = "password";
+        private static readonly string STR_LOGIN_BUTTON_XPath = "//*[text()='Iniciar sesion' or text()='Sing in']";
+
+        //PAGE ELEMENT OBJECT
+        private static IWebElement objUsernameText => _objDriver.FindElement(By.Id(STR_USSERNAME_TEXTFIELD_ID));
+        private static IWebElement objPasswordText => _objDriver.FindElement(By.Id(STR_PASSWORD_TEXTFIELD_ID));
+        private static IWebElement objLogin_Button => _objDriver.FindElement(By.XPath(STR_LOGIN_BUTTON_XPath));
+
+        //GET ELEMENT METHODS
+        public IWebElement GetUsernameField()
+        {
+            return objUsernameText;
+        }
+        public IWebElement GetUserpasswordField()
+        {
+            return objPasswordText;
+        }
+        public IWebElement GetLoginButton()
+        {
+            return objLogin_Button;
+        }
+
+        //PAGE ELELEMET ACTION
+        public void fnEnterUsername(string pstrUsername)
+        {
+            objUsernameText.Clear();
+            objUsernameText.SendKeys(pstrUsername);
+        }
+
+    }
+}
---
+++ LinkedIn_LoginPage/Test Cases/LinkedIn_LoginTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedIn_LoginPage.Base_Files;
+using LinkedIn_LoginPage.Page_Object;
+using NUnit.Framework;
+
+namespace LinkedIn_LoginPage.Test_Cases
+{
+    class LinkedIn_LoginTest : BaseTest
+    {
+        public static Linkedin_LoginPageModel objLogin;
+
+        [Test]
+
+        public void LinkedIn_login()
+        {
+            objLogin = new Linkedin_LoginPageModel(driver);
+            objLogin.fnEnterUsername(username);
+        }
+    }
+}
