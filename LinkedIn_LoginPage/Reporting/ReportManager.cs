a8e5ddd45c48c4f808d10d88bb32077cc599286c
Josetziu4 <47648101+Josetziu4@users.noreply.github.com>
2020-01-07 17:05:49 -0600

    ReportManager

---
+++ LinkedIn_LoginPage/Reporting/ReportManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LinkedIn_LoginPage.Reporting
+{
+    class ReportManager
+    {
+        [Test]
+        public void fnGetReportPath()
+        {
+            string strExecutionPath = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string strBaseDirectory = strExecutionPath.Substring(0, strExecutionPath.IndexOf("bin"));
+            strBaseDirectory = new Uri(strBaseDirectory).LocalPath;
+
+            string strReportDirectory = strBaseDirectory + "ExtentReports";
+            if (!Directory.Exists(strReportDirectory))
+            {
+                Directory.CreateDirectory(strReportDirectory);
+            }
+
+            string strReportPath = strReportDirectory + "\\ExtentReport_" + DateTime.Now.ToString("MMddyyyy_HHmmss") + ".html";  //ExtentReport_MMDDYYYY_HHMMSS.html
+        }
+    }
+}
